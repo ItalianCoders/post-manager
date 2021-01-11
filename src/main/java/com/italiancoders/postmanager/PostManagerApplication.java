24fbd2ab68ea889b151d1297977dc740182c529e
gioboa <giorgiob.boa@gmail.com>
2021-01-11 12:29:42 +0100

    dev: added starting log

--- src/main/java/com/italiancoders/postmanager/PostManagerApplication.java
+++ src/main/java/com/italiancoders/postmanager/PostManagerApplication.java
@@ -1,14 +1,17 @@
 package com.italiancoders.postmanager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@Slf4j
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PostManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostManagerApplication.class, args);
+		log.info("---- POST-MANAGER STARTED ----");
 	}
 
 }
